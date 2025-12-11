8a92e36c4109f1acf84cf20ec4c9eb644f72b0ec
aditya nikam <adiityaa.nikam@gmail.com>
2025-12-12 01:30:08 +0530

    Time: 14 ms (31.22%), Space: 46.8 MB (10.62%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+    }
+}
+
+
+
