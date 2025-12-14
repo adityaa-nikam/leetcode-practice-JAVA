4ff260bb3af4a0cc630c4d35dc6c6e1b8472f650
aditya nikam <adiityaa.nikam@gmail.com>
2025-12-14 22:39:45 +0530

    Time: 989 ms (5%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,11 @@
-SELECT e2.name AS Employee
-From Employee e1
-INNER JOIN Employee e2 ON e1.id = e2.managerId
-WHERE e1.salary < e2.salary
+SELECT e1.name AS Employee
+FROM Employee as e1
+WHERE e1.salary > ( SELECT e2.salary 
+                    FROM Employee e2 
+                    WHERE e2.id = e1.managerId )
+
+-- ALTERNATE OUTPUT
+-- SELECT e2.name AS Employee
+-- From Employee e1
+-- INNER JOIN Employee e2 ON e1.id = e2.managerId
+-- WHERE e1.salary < e2.salary
